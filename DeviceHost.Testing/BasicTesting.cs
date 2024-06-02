01320f0dba8468bde98ee58426120e8123737012
Kristian Hennings <krhe@inventors.dk>
2024-06-02 15:17:22 +0200

    Started implementing unit tests for the API

--- DeviceHost.Testing/BasicTesting.cs
+++ DeviceHost.Testing/BasicTesting.cs
@@ -7,40 +7,37 @@ namespace DeviceHost.Testing
     [TestClass]
     public class BasicTesting
     {
-        [TestMethod]
-        public void LoadScript()
+        private void TestScript(string script)
         {
-            var script = TestUtility.GetScript("Open.txt");
-            Console.WriteLine(script);
-        }
-
-        [TestMethod]
-        public void T01_Ping()
-        {
-            string serverIp = "127.0.0.1"; // IP address of the server
-            int serverPort = 9797;        // Port of the server
-
             try
             {
-                using TcpClient client = new(serverIp, serverPort);
-                using NetworkStream stream = client.GetStream();
+                string messageToSend = TestUtility.GetScript(script);
+                Console.WriteLine("Sent:");
+                Console.WriteLine(messageToSend);
+                Console.WriteLine();
 
-                // Send a message to the server
-                string messageToSend = "Hello, Server!";
-                byte[] dataToSend = Encoding.UTF8.GetBytes(messageToSend);
-                stream.Write(dataToSend, 0, dataToSend.Length);
-                Console.WriteLine("Sent: {0}", messageToSend);
-
-                // Receive the response from the server
-                byte[] dataToReceive = new byte[256];
-                int bytesRead = stream.Read(dataToReceive, 0, dataToReceive.Length);
-                string response = Encoding.UTF8.GetString(dataToReceive, 0, bytesRead);
-                Console.WriteLine("Received: {0}", response);
+                string response = TestUtility.Send(messageToSend);
+                Console.WriteLine("Received:");
+                Console.WriteLine(response);
+                Console.WriteLine();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Exception: {0}", ex.Message);
             }
         }
+
+        [TestMethod]
+        public void T01_GetPorts() => TestScript("GetPorts.txt");
+
+        [TestMethod]
+        public void T02_Open() => TestScript("Open.txt");
+
+        [TestMethod]
+        public void T03_Ping() => TestScript("Ping.txt");
+
+        [TestMethod]
+        public void T04_Close() => TestScript("Close.txt");
+
     }
 }
--- DeviceHost.Testing/TestUtility.cs
+++ DeviceHost.Testing/TestUtility.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.Net.Sockets;
 
 namespace DeviceHost.Testing
 {
     public class TestUtility
     {
-        public static string? GetScript(string name)
+        private readonly static string serverIp = "127.0.0.1"; // IP address of the server
+        private readonly static int serverPort = 9797;        // Port of the server
+
+        public static string GetScript(string name)
         {
             if (Assembly.GetExecutingAssembly().Location is not string assemblyFileName)
                 throw new InvalidOperationException("Invalid path to assembly");
@@ -26,6 +30,23 @@ namespace DeviceHost.Testing
             var scriptFilePath = Path.Combine(scriptPath, name);
 
             return File.ReadAllText(scriptFilePath);
+        }
+
+        public static string Send(string command)
+        {
+            using TcpClient client = new(serverIp, serverPort);
+            using NetworkStream stream = client.GetStream();
+
+            // Send a message to the server
+            byte[] dataToSend = Encoding.UTF8.GetBytes(command);
+            stream.Write(dataToSend, 0, dataToSend.Length);
+
+            // Receive the response from the server
+            byte[] dataToReceive = new byte[1024];
+            int bytesRead = stream.Read(dataToReceive, 0, dataToReceive.Length);
+            string response = Encoding.UTF8.GetString(dataToReceive, 0, bytesRead);
+
+            return response;
         }
     }
 }
